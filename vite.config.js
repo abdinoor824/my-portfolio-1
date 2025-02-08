c7bc79ff3f6f35fd2bb4fe6d8acf7f8b3198850d
abdinoor <abdinurkune@gmail.com>
2025-02-08 15:08:52 +0300

    changed

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 10000, // Use the port from the environment variable or default to 3000
     host: '0.0.0.0', 
-    allowedHosts: ['sport1-16455.onrender.com'], 
+    allowedHosts: ['my-portfolio-1.onrender.com'], 
 },
 })
